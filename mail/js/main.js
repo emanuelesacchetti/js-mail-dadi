2c3568906d6bb1663705e79519f39827c02bbdb1
emanuele sacchetti <manusacco92@gmail.com>
2023-02-27 15:46:17 +0100

    problemi con il ciclo for di mail

---
+++ mail/js/main.js
@@ -0,0 +1,16 @@
+//creo un array con le email accettate
+let emailAccettate = ['trallallero@gmail.com', 'ucciuccitarallucci@gmail.com', 'selallero@gmail.com', 'anvediquesto@gmail.com', 'machevoi@gmail.com']; 
+
+//creo il prompt per richiedere la email all'utente
+const emailInserita = prompt("Digita la tua e-mail Google facendo attenzione che che termini con '@gmail.com'");
+
+console.log(emailAccettate.length);
+
+//creo un ciclo for che si ripeta per il numero di elementi presenti nell'array
+//Ogni volta che la email dell'utente è diversa dal numero d'insieme degli elementi della lista fai qualcosa
+
+for(i = 0; i < emailAccettate.length; i++){
+    if (emailInserita != emailAccettate.length[i]){
+        console.log('ciao');
+    }
+}
